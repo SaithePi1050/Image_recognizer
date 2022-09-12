e42d79e3f646ffc491a4c0cce42dea587dbbe0b7
SaithePi <89380734+SaithePi1050@users.noreply.github.com>
2022-09-12 18:27:00 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -12,15 +12,37 @@ function take_snapshot()
 {
     Webcam.snap(function (data_uri) 
     {
-        document.getElementById("Result").innerHTML = 'img id="capture_img" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="capture_img" src="'+data_uri+'"/>';
     });
 }
 
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zYlw_C1Qr/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/i2LliJr-t/model.json', modelLoaded);
 
 function modelLoaded() 
 {
     console.log("Model has been loaded succesful");
 }
 
+function check() 
+{
+    img = document.getElementById("capture_img");
+    classifier.classify(img, result);
+}
+
+function result(error, results) 
+{
+    if (error) {
+        console.log(error); 
+    } else {
+        console.log(results);
+    } 
+    
+    document.getElementById("objects-name").innerHTML = results[0].label;
+    document.getElementById("objects-accuracy").innerHTML = results[0].confidence.toFixed(2);
+
+    var save = window.speechSynthesis;
+    var data = "This is" + results[0].label;
+    var utter = new SpeechSynthesisUtterance(data);
+    save.speak(utter);
+}
